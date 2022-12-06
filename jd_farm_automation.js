a1721140a4eb2cb56111b1b6ef6a2956f8d898cc
yanjuan <yanjuan@xinc818.group>
2022-12-06 21:07:12 +0800

    del js

--- jd_farm_automation.js
+++ jd_farm_automation.js
@@ -1,4 +1,4 @@
-//20 8 10 4 * jd_farm_automation.js
+//20 8,21 * * * jd_farm_automation.js
 
 console.log('默认种2级，如需调整请设置变量 M_JD_FARM_LEVEL\n使用率不高，指定（desi）账号运行\n')
 const {Env} = require('./function/magic');
