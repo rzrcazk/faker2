288b8f39e497ada95f7be007722db8b52a507cd6
rzrcazk <sulongliu6@gmail.com>
2023-01-04 16:55:04 +0800

    txzj 关注有礼 加代理

--- jd_txzj_collect_item.js
+++ jd_txzj_collect_item.js
@@ -9,7 +9,8 @@ export jd_txzj_collect_item_num="15"                   执行前多少个号  �
 
 cron "1 1 1 1 1" jd_txzj_collect_item.js
 */
-
+require("global-agent/bootstrap");
+global.GLOBAL_AGENT.HTTP_PROXY="http://172.18.1.13:8080";
 const $ = new Env('txzj 关注有礼')
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 const notify = $.isNode() ? require('./sendNotify') : '';
