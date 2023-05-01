435d7f20417d0531233aae62d22ab5b651a750bd
rzrcazk <sulongliu6@gmail.com>
2023-05-01 21:08:12 +0800

    助力次地址修改为自己的

--- jd_fruit.js
+++ jd_fruit.js
@@ -25,8 +25,8 @@ export DO_TEN_WATER_AGAIN="" 默认再次浇水
 
 */
 
-// require("global-agent/bootstrap");
-// global.GLOBAL_AGENT.HTTP_PROXY="http://172.18.1.13:8080";
+require("global-agent/bootstrap");
+global.GLOBAL_AGENT.HTTP_PROXY="http://172.18.1.13:8080";
 const $ = new Env('东东农场助力池版');
 let cookiesArr = [],
     cookie = '',
